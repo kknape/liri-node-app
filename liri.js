c84b1c38905aa2b4dadfe6d621e5e93ca2bb999a
k2k <kadonna@wonderkul.com>
2019-08-03 00:18:31 -0500

    Got the Concert call working and printing results. Added Moment format to datetime stamp. Still having trouble getting the data out of Spotify. Call is coming back but can't get print the values.

--- liri.js
+++ liri.js
@@ -1,15 +1,15 @@
 //My Liri Bot
 
-//Liri can take in these commands:
-// concert-this
-// spotify-this-song
-// movie-this
-// do-what-it-says
+    //Liri can take in these commands:
+    // concert-this
+    // spotify-this-song
+    // movie-this
+    // do-what-it-says
 
-//To answer these commands, retrieve data using the `axios` package from these apis
-//[OMDB API](http://www.omdbapi.com)
-//[Bands In Town API](http://www.artists.bandsintown.com/bandsintown-api)
-//Spotify
+    //To answer these commands, retrieve data using the `axios` package from these apis
+    //[OMDB API](http://www.omdbapi.com)
+    //[Bands In Town API](http://www.artists.bandsintown.com/bandsintown-api)
+    //Spotify
 //END REQUIREMENTS
 
 
@@ -23,7 +23,6 @@
     //spotify
     var Spotify = require('node-spotify-api');  
     var spotify = new Spotify(keys.spotify);
-    console.log(spotify);
     //moment
       var moment = require("moment");
         moment().format();
@@ -34,9 +33,6 @@
       //inquirer to get user inputs
       const inquirer = require("inquirer");
       
-debugger;
-
-
 //User makes a request of Liri
 
 var question1 = [
@@ -82,15 +78,8 @@ var questionDoIt = [
 
         inquirer.prompt(question1).then(answer1 => {
           console.log(answer1.heyLiri)
-        
-   //     );
- //   answers.heyLiri
-    //  var heyLi = answers.askLiri;
-    //  console.log(JSON.stringify(answers, null, ' '));
-    
-  
-//debugger;
-  // Performing the appropriate operation based on user's selection
+
+  // Run function depending on user's selection; 
      if  (answer1.heyLiri === "concert-this") {
             concert();
       } 
@@ -111,49 +100,70 @@ var questionDoIt = [
  //call concert function to find a local event for a band; uesr inputs band name, run call to band api
        function concert(){
           inquirer.prompt(questionConcert).then(answer2=>{
-            var searchConcert = (answer2.whatConcert)
-          //  console.log(searchConcert);             
+            var searchConcert = (answer2.whatConcert)            
 /*<<<<<<<<Working Bands in Town call START*>>>>>>>*/
               axios
                 .get("https://rest.bandsintown.com/artists/"+ searchConcert + "/events?app_id=codingbootcamp")
                 .then(function(response){
-                  console.log(response.data);
-                  
-                var eventResults=(response.data);
-               //  console.log(eventResults.process.argv[3]);
-                var eventArr = Array.from(eventResults);
-                var venueInfo = eventResults.slice(2,3);
-                console.log(venueInfo);
-               //   console.log(eventArr);
-               //   console.log(eventArr[3]);
-
-                //  console.log(response.data.venue.city);
-                //  console.log(response.data.venue.datetime);
-//name of venue
-//venue location
-//date of the event (use moment to format as "MM/DD/YYYY")
-
+                  for (var i = 0; i < response.data.length; i++) {
+                        //name of venue
+                        var venName = (JSON.stringify(response.data[i].venue.name));
+                          //remove the quotes from the response data
+                          var venueNameF = venName.substr(1);
+                          var venueNameL = venueNameF.slice(0, -1);
+                          console.log("\n" + venueNameL);
+                        //venue location
+                        var location = (JSON.stringify(response.data[i].venue.city + ", " + response.data[0].venue.country));
+                        //remove the quotes from the response data
+                        var locF = location.substr(1);
+                        var locL = locF.slice(0, -1);
+                        console.log(locL);
+                        //get the date of the event
+                        var date = (JSON.stringify(response.data[i].datetime));
+                        //get the day, month, year from the full datetime "2019-10-26T19:30:00"
+                        var strDate = date.substring(1, 11);    
+                        //convert to MM/DD/YYYY using moment                   
+                        var showDate =  moment(strDate).format("MM/DD/YYYY");
+                       console.log(showDate);
+                  }
                 }) 
                 .catch(function(err) {
                   console.error('Error occurred: ' + err); 
                 });
-              });
-            }   
+              })
+            };
+           
 /*<<<<<<<<working bandsintown call END*>>>>>>>*/        
       
 //call song function to get details about a song input by the user, call to spotify api for details
     function song(){
           inquirer.prompt(questionSong).then(answer3=>{
             var searchSong = (answer3.whatSong)
-          //  console.log(searchSong);
-            spotify.search({ type: 'track', query: searchSong, limit: 10 }, function(err, data) {
-              if (err) {
-                return console.log('Error occurred: ' + err);
-              }
-              console.log(data); 
-            });
-        });      
-      }
+              console.log(searchSong);
+
+            spotify.search({ type: 'track', query: searchSong, limit: 5 })
+              .then(function(response){ 
+              //  console.log(response);
+              // console.log(JSON.stringify(data, null, 2)); 
+             //     for (var i = 0; i < response.data.length; i++) {
+                      //name of Artist(s)
+                     console.log(JSON.stringify(response));
+                    })
+                  
+                      //The song's name
+              //        console.log(JSON.stringify(response.data[0].name));
+                      //A preview link of the song from Spotify
+               //       console.log(JSON.stringify(response.data[0].external_urls.spotify));
+                      //The album that the song is from
+              //        console.log(JSON.stringify(response.data[0].album.name));
+     
+                      .catch(function(err) {
+                        console.log(err);
+                        });
+                   })
+                };
+              
+              
 //call fucntion to get user input of movie, get details of movie from omdb api      
       function movie(){
             inquirer.prompt(questionMovie).then(answer4=>{
@@ -162,18 +172,18 @@ var questionDoIt = [
                    axios
                     .get("http://www.omdbapi.com/?t=" + searchMovie + "&apikey=trilogy")
                     .then(function(response){
-                    console.log(response.data);
-                    console.log(response.data.Title);
-                    console.log(response.data.Year);
-                    console.log(response.data.imdbRating);
-                    console.log(response.data.Ratings.Source.Value);
+                 //   console.log(response.data);
+                    console.log("Movie: " + response.data.Title);
+                    console.log("Year released: " + response.data.Year);
+                    console.log("IMDB rating: " + response.data.imdbRating);
+                    console.log("Rotten Tomatoes score: " + response.data.Ratings[1].Value);
                     })
                     .catch(function(err) {
                       console.error('Error occurred: ' + err); 
                     });
                   });
                 }
-/*<<<<<<<<working OMDB call END*>>>>>>>*/  
+/*<<<<<<<<Movie END>>>>>>>*/  
 
 //call fucntion  for "do what it says" which reads radom file which has [spotify-this-song,"I Want it That Way" ]
       function doIt(){
